75eca2b8c2e142cdf562875750f26ef4f92b23e0
F-linty <1015.flinty@gmail.com>
2026-03-09 09:45:36 +0800

    书写初始化文件

---
+++ dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const server = (port) => {
+    console.log(`服务启动成功，端口:${port}`);
+};
+server(3000);
+//# sourceMappingURL=test.js.map
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+const server = (port: number) => {
+  console.log(`服务启动成功，端口:${port}`);
+};
+server(3000);
